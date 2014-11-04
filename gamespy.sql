8a3981ea4962b1ffd1859641b087c0c074035c8b
Alex Sims <alexander.sims92@gmail.com>
2014-11-04 14:06:22 +0000

    Built consoles table

--- gamespy.sql
+++ gamespy.sql
@@ -1,6 +1,31 @@
 CREATE TABLE consoles
 (
-
+	console_id 	NUMBER(11)
+				CONSTRAINT consoles_console_id_pk
+						PRIMARY KEY
+				CONSTRAINT consoles_console_id_nn
+						NOT NULL,
+	manufac     NUMBER(11) 
+				CONSTRAINT consoles_manufac_fk
+						REFERENCES manufacturers(manufac_id) ON DELETE SET NULL
+				CONSTRAINT consoles_manufac_nn
+						NOT NULL,
+				CONSTRAINT consoles_manufac
+	name		VARCHAR2(50)
+				CONSTRAINT consoles_name_nn
+						NOT NULL,
+	release		DATE
+				CONSTRAINT consoles_release_nn
+						NOT NULL,
+	capacity	NUMBER(8) DEFAULT(0)
+				CONSTRAINT consoles_capacity_nn
+						NOT NULL,
+	rr_price    NUMBER(10)
+				CONSTRAINT consoles_rr_price_nn
+						NOT NULL,
+	description VARCHAR2(5000)
+				CONSTRAINT consoles_description_nn
+						NOT NULL
 )
 
 /*-----------------------------------------------------------------
